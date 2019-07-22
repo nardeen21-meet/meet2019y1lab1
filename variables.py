e1da3e8260c3c4d40017a6adfffed5ec209ca794
nardeen21-meet <nardeen21@meet.mit.edu>
2019-07-22 14:27:56 +0300

    Stopping here

---
+++ variables.py
@@ -0,0 +1,9 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 5
+>>> b = 10
+>>> a =b
+>>> b = a
+>>> my_age = 15
+>>> print ('I am ' + my_age = 'years old')
